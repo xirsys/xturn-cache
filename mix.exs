165ebb309e2915dbc77bd25abad80474c60b3cea
Lee Sylvester <lee.sylvester@gmail.com>
2019-07-19 14:30:32 +0100

    Update mix file

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule XturnCache.MixProject do
+defmodule Xirsys.XTurn.Cache.MixProject do
   use Mix.Project
 
   def project do
@@ -6,23 +6,36 @@ defmodule XturnCache.MixProject do
       app: :xturn_cache,
       version: "0.1.0",
       elixir: "~> 1.6.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Cache with TTL for the XTurn server.",
+      source_url: "https://github.com/xirsys/xturn-cache",
+      homepage_url: "https://xturn.me",
+      package: package(),
+      docs: [
+        extras: ["README.md", "LICENCE"],
+        main: "readme"
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    []
+  end
+
+  defp package do
+    %{
+      maintainers: ["Jahred Love"],
+      licenses: ["Apache 2.0"],
+      organization: ["Xirsys"],
+      links: %{"Github" => "https://github.com/xirsys/xturn-cache"}
+    }
   end
 end
